7418b8fd875ed5eb6ae9451c573d3fd549844014
Filniy <86433836+Filniy@users.noreply.github.com>
2024-08-06 18:22:04 +0300

    Fourth homework done

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+#def remove(string):
+    #return string.replace(" ", "") #space remover
+
+my_string = str(input("blabla, type your string now"))
+print("LetterCount:",len(my_string))
+print("Uppercase:",my_string.upper())
+print("Lowercase:",my_string.lower())
+#print(remove(my_string))
+print("No Spaces:",my_string.replace(" ",""))
+print("First letter:",my_string[1])
+print("Last letter",my_string[-1])
