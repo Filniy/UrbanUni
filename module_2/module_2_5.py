5a80d5c75bea1cba8e1ad79390dfb12a9786bb49
Filxyz <86433836+Filniy@users.noreply.github.com>
2024-09-22 11:14:28 +0000

    2.5 HW done

---
+++ module_2/module_2_5.py
@@ -0,0 +1,16 @@
+# n - кол-во списков в матрице
+# m - кол-во элементов в списке
+# value - значение, которое будет присвоено каждому элементу матрицы
+def get_matrix(n,m,value):
+  matrix=[]
+  for i in range(n):
+    matrix.append([])
+    for j in range(m):
+      matrix[i].append(value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
