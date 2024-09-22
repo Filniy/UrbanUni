09e11835b5abbd87513c13c007f98f16cd801818
Filxyz <86433836+Filniy@users.noreply.github.com>
2024-09-22 16:44:36 +0000

    starting 3.2 HW

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -29,6 +29,7 @@ print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 print(calls)
+#Done
 
 
   
