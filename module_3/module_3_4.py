3948d64b363a90090620baeac23619c493981b22
Filxyz <86433836+Filniy@users.noreply.github.com>
2024-09-24 18:17:36 +0000

    HW 3.4 done

---
+++ module_3/module_3_4.py
@@ -0,0 +1,11 @@
+def single_root_words(root_word,*other_words):
+  same_words=[]
+  for word in range(len(other_words)):
+    if root_word.lower() in other_words[word].lower() or other_words[word].lower() in root_word.lower():
+      same_words.append(other_words[word])
+  return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
