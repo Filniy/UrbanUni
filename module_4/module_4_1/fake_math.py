7268aaf18407fcafd43e92f0ece7bcd5bf6783f2
Filxyz <86433836+Filniy@users.noreply.github.com>
2024-10-08 14:25:23 +0000

    HW 4.1 Done

---
+++ module_4/module_4_1/fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+  if first == 0 or second == 0:
+    return str('Ошибка')
+  else:
+    return (first/second)
---
+++ module_4/module_4_1/true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide(first, second):
+  if first == 0 or second == 0:
+    return inf
+  else:
+    return (first/second)
+  
+  
