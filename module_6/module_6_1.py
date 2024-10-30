eb035993d0cdf54981af8803d8e3d65ab32e6d11
Filniy <86433836+Filniy@users.noreply.github.com>
2024-10-30 17:36:15 +0300

    HW 6.1 Done

---
+++ module_6/module_6_1.py
@@ -0,0 +1,45 @@
+class Animal:
+    alive = True
+    fed = False
+    
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} сьел {food.name}')
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+class Plant:
+    edible = False
+
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
